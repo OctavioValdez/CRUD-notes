2f846f285d76b959cd399cf833ab075e9eb4435e
Octavio Valdez <90404916+OctavioValdez@users.noreply.github.com>
2024-11-28 04:50:09 -0600

    notas

--- crud.py
+++ crud.py
@@ -15,8 +15,10 @@ CORS(app)
 
 bucket_name = os.getenv('BUCKET_NAME')
 aws_region = os.getenv('REGION')
+aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
 
-bucket = boto3.client('s3', aws_region)
+bucket = boto3.client('s3', aws_access_key_id, aws_secret_access_key, aws_region)
 
 def get_db_connection():
     connection = pymysql.connect(
